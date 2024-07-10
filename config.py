2118c34a16daf2feb3b5d23ecb9c86a74576c0d5
Jesse Kleemann <j.kleemann@tu-bs.de>
2024-07-10 22:57:50 +0200

    add home assistant config

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import json
 from pathlib import Path
 
 import yaml
 
 
 def get_config_local(filename: Path) -> dict:
-    with open(filename, 'r') as file:
+    with open(filename) as file:
         try:
             return yaml.safe_load(file)
         except yaml.YAMLError as e:
@@ -12,7 +13,29 @@ def get_config_local(filename: Path) -> dict:
             return {'error': str(e)}
 
 
-try:
-    config: dict = get_config_local(Path('/config/config.yaml'))
-except FileNotFoundError:
-    config = get_config_local(Path('config.yaml'))
+def get_first_config() -> dict:
+    files = [
+        Path('/config/config.yaml'),
+        Path('config.yaml'),
+        Path('config.sh10rt.example.yaml')
+    ]
+    for file in files:
+        if file.exists():
+            loaded_config = get_config_local(file)
+            break
+    else:
+        raise FileNotFoundError
+    options_file = Path('/data/options.json')
+    if options_file.exists():
+        with open(options_file) as file:
+            options = json.load(file)
+        for key in options:
+            if isinstance(options[key], str):
+                if len(options[key]) > 0:
+                    loaded_config[key] = options[key]
+            elif isinstance(options[key], int):
+                loaded_config[key] = options[key]
+    return loaded_config
+
+
+config: dict = get_first_config()
